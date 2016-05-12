fc7ae22bdefd8b9eb1c48458ffd31e9a70e8c11d
florianduport@gmail.com <florianduport@gmail.com>
2016-05-12 22:19:25 +0200

    networking ok

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TarsJS
 Project RESET !
+
+
+nginx : 172.16.0.10
+sails : 172.16.0.11
+mongo : 172.16.0.12
--- run.sh
+++
@@ -1,3 +0,0 @@
-cd db && docker-compose run -d db
-cd ../api && docker-compose run -d api
-cd ../front && docker-compose run front
